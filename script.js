afba065f677598a5541408b92a973f0e2d468b66
Stephen Pleasant <stephenpleasant1@gmail.com>
2023-05-10 14:29:01 -0600

    first commit

--- script.js
+++ script.js
@@ -2,16 +2,43 @@ const email = document.getElementById('email')
 const submit = document.getElementById('submit')
 const form = document.getElementById('form')
 
-
+function showError(errorElement, ){
+    document.querySelector("."+errorElement);
+}
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(email.value);
+   
+    validateInputs();
+});
+
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error');
+
+    errorDisplay.innerText = message;
+    inputControl.classList.add('error');
+    inputControl.classList.add('success');
+
+    errorDisplay.innerText = '';
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+    const emailValue = email.Value.trim();
+
+    if(emailValue === '') {
+        setError(email, 'Please is required')
+    }else if (!isValidEmail(emailValue)) {
+        setError(email, 'provide a valid email address')
     
-    let message = []
-    if  (email.value === '' || email.value == null) {
-        message.push('email is required')
+    }else {
+        setSuccess(email);
     }
-    if (email.value !== '@gmail.com') {
-        message.push('please provide a valid email');
-    }
-})
+};
